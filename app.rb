681d2637b16142094cdcadb1cf74b68e1051cd8d
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-24 22:40:41 +0000

    Added conversion page

--- app.rb
+++ app.rb
@@ -38,10 +38,8 @@ get("/:from_currency") do
 
   raw_data = HTTP.get(api_url)
 
-  # convert the raw request to a string
   raw_data_string = raw_data.to_s
 
-  # convert the string to JSON
   parsed_data = JSON.parse(raw_data_string)
 
   # get the symbols from the JSON
@@ -50,4 +48,22 @@ get("/:from_currency") do
   #@original_currency_order = @symbols.index(@original_currency)
 
   erb(:symbol_page)
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATES_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # some more code to parse the URL and render a view template
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+
+  parsed_data = JSON.parse(raw_data_string)
+
+  @conversion_result = parsed_data.fetch("result")
+
+  erb(:conversion)
 end
